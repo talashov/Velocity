08c0e74164b4e25b6e8d3e9d0736fed463e34ac4
Nikita <ntalashov@gmail.com>
2022-06-27 18:49:00 +0300

    slider works

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-const openPopUp = document.getElementById("open");
-const closePopUp = document.getElementById("close");
-const popUp = document.getElementById("popup");
+$(function () {
+  const openPopUp = document.getElementById("open");
+  const closePopUp = document.getElementById("close");
+  const popUp = document.getElementById("popup");
 
-openPopUp.addEventListener("click", function (e) {
-  e.preventDefault();
-  popUp.classList.add("active");
-});
+  openPopUp.addEventListener("click", function (e) {
+    e.preventDefault();
+    popUp.classList.add("active");
+  });
 
-openPopUp.addEventListener("click", function (e) {
-  e.preventDefault();
-  openPopUp.classList.add("hiden");
-});
+  openPopUp.addEventListener("click", function (e) {
+    e.preventDefault();
+    openPopUp.classList.add("hiden");
+  });
 
-closePopUp.addEventListener("click", () => {
-  popUp.classList.remove("active");
-});
+  closePopUp.addEventListener("click", () => {
+    popUp.classList.remove("active");
+  });
 
-closePopUp.addEventListener("click", () => {
+  closePopUp.addEventListener("click", () => {
     openPopUp.classList.remove("hiden");
   });
+
+  // $('.works__slider').slick();
+  $('.works__slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+});
